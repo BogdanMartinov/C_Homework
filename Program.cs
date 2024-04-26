ff0679f49576bce5b0b56c92979d6664e85aa90e
Bogdan <martinovbogdan92@yandex.ru>
2024-04-26 14:19:02 +0300

    Решение первой задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача1. Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа
+// в промежутке от M до N.
+
+// void FromToMandN (int m, int n)
+// {
+//     if (m>n)
+//     {
+//         FromToMandN(m-1,n);
+//         System.Console.Write(m + " ");
+//     }
+//     else if (n>m)
+//     {
+//         FromToMandN(n-1,m);
+//         System.Console.Write(n + " ");
+//     }
+//     else System.Console.Write(m + " ");
+// }
+
+// FromToMandN(6,2);
